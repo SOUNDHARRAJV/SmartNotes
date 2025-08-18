0c6c7bae6840f9e29330e14eeec6f13e6ba59237
SOUNDHAR RAJ V <soundharrajvellingiri@gmail.com>
2025-08-18 15:14:29 +0530

    Update Dashboard.tsx

--- project/src/components/Dashboard.tsx
+++ project/src/components/Dashboard.tsx
@@ -18,7 +18,7 @@ import {
 const LM_STUDIO_API =
   window.location.hostname === "localhost"
     ? "http://localhost:1234/v1/chat/completions"
-    : "https://ugly-col-provided-romania.trycloudflare.com/v1/chat/completions";
+    : "https://press-computers-parents-orange.trycloudflare.com/v1/chat/completions";
 ";
 
 const MODEL_NAME = "AI_SRK_CHATBOT";
